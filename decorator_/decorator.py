7f45bd683785fca87ce0e7323b8f88b6d3531bc9
chenguang <312197153@qq.com>
2018-01-25 11:14:54 +0800

    写了一些没用的东西

---
+++ decorator_/decorator.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/21 15:44
+# @author  : slark
+# @File    : decorator_.py
+# @Software: PyCharm
+
+import functools
+
+
+# 检查函数的参数，如果参数不是整数，则报错
+def decorator(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        kwargs_values = [value for value in kwargs.values()]
+        for arg in list(args) + kwargs_values:
+            if not isinstance(arg, int):
+                raise TypeError("{0} only accept integers as arguments ".format(func.__name__))
+
+        return func(*args, **kwargs)
+    return inner
+
+
